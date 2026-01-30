c467eff8395b184e26a53d82be64833f44a2f3ef
Villiamremote <willyp@hotmail.se>
2026-01-30 11:39:11 +0100

    Added Reservation Controller and dto.

---
+++ Application/DTOs/Reservations/SetCartReservationDto.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Application.DTOs.Reservations;
+
+public sealed record SetCartReservationDto(
+    string CartId,
+    int ProductId,
+    int Quantity,
+    int ReservationTtlMinutes = 30
+    );
