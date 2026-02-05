1da6af27f7bbb1f443129f0c0b6cafb897e51420
Villiamremote <willyp@hotmail.se>
2026-02-05 11:44:32 +0100

    Readded IntegrationTest

---
+++ IntegrationTests/Contracts/Common/PagedResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntegrationTests.Contracts.Common;
+
+public sealed class PagedResult<T>
+{
+    public List<T> Items { get; set; } = [];
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages { get; set; }
+    public int TotalCount { get; set; }
+}
---
+++ IntegrationTests/Features/Products/GetProductsTests.cs
@@ -0,0 +1,19 @@
+﻿
+
+using IntegrationTests.Infrastructure;
+
+namespace IntegrationTests.Features.Products;
+
+[Collection("db")]
+public sealed class GetProductsTests : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly HttpClient _client;
+    private readonly DbFixture _db;
+
+    public GetProductsTests(CustomWebApplicationFactory factory, DbFixture db)
+    {
+        _client = factory.CreateClient();
+        _db = db;
+    }
+
+}
