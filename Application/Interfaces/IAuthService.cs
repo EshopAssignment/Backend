e78b569fde21fc9189a01ddaa60bd913ee9f86a3
Villiamremote <willyp@hotmail.se>
2026-02-23 14:46:14 +0100

    Added authService.

---
+++ Application/Interfaces/IAuthService.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.Eventing.Reader;
+using Application.DTOs.Auth;
+
+namespace Application.Interfaces;
+
+public interface IAuthService
+{
+    Task<(bool Ok, IDictionary<string, string[]>? Errors)> RegisterAsync(RegisterDto dto, CancellationToken ct);
+    Task<(bool Ok, int? UserId, TokenPair? Pair)> LoginAsync(LoginDto dto, CancellationToken ct);
+    Task<(bool Ok, int? UserId, TokenPair? Pair)> RefreshAsync(int uid, string refreshToken, CancellationToken ct);
+
+    Task ForgotPasswordAsync(string email, CancellationToken ct);
+    Task<(bool Ok, String? Error)> ResetPasswordAsync(string email, string toke, string newPassword, CancellationToken ct);
+
+    Task ResendEmailVerificationAsync(string email, CancellationToken ct);
+    Task<(bool Ok, string? Error)> ConfirmEmailAsync(int userId, string token, CancellationToken ct);
+}
