7232a734036c905b8c87bd2e1ce0b922ec385814
Villiamremote <willyp@hotmail.se>
2026-03-19 16:21:52 +0100

    Added Controller and interface.

---
+++ Api/Controllers/CustomRequestsController.cs
@@ -0,0 +1,29 @@
+﻿using Application.DTOs.Order;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CustomRequestsController(ICustomRequestService customRequestService) : ControllerBase
+{
+    private readonly ICustomRequestService _customRequestService = customRequestService;
+
+    [HttpPost]
+    [RequestSizeLimit(10_000_000)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Create([FromForm] CreateCustomRequestForm form, CancellationToken ct)
+    {
+        var result = await _customRequestService.CreateAsync(form, ct);
+
+        if (!result.Ok)
+            return BadRequest(new {message = result.Error});
+
+        return Ok(new { message = "Förfrågan mottagen" });
+    }
+}
+
+
---
+++ Application/Interfaces/ICustomRequestService.cs
@@ -0,0 +1,8 @@
+﻿using Application.DTOs.Order;
+
+namespace Application.Interfaces;
+
+public interface ICustomRequestService
+{
+    Task<(bool Ok, string? Error)> CreateAsync(CreateCustomRequestForm form, CancellationToken ct);
+}
