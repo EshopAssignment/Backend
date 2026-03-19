463c23d041e71a989906349b2c66570f6ca47eeb
Villiamremote <willyp@hotmail.se>
2026-03-19 16:13:55 +0100

    Added RequestDto

---
+++ Application/DTOs/Order/CreateCustomRequestForm.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.DTOs.Order;
+
+
+public sealed record CreateCustomRequestForm
+{
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; init; } = "";
+
+    [Required]
+    [EmailAddress]
+    [MaxLength(200)]
+    public string Email { get; init; } = "";
+
+    [Phone]
+    [MaxLength(50)]
+    public string Phone { get; init; } = "";
+
+    [Required]
+    [MaxLength(4000)]
+    public string Message { get; init; } = "";
+
+    public IFormFile? File { get; init; }
+}
