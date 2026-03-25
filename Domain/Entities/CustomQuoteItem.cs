b66504773109e84a218f55f169d016de1a105879
Villiamremote <willyp@hotmail.se>
2026-03-25 13:58:01 +0100

    Added entites and enums

---
+++ Domain/Entities/CustomQuoteItem.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Domain.Entities;
+
+public class CustomQuoteItem
+{
+    public int Id { get; set; }
+
+    public int CustomQuoteId { get; set; }
+    public CustomQuote CustomQuote { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+    public int Quantity { get; set; }
+
+    public int VatRatePercent { get; set; }
+    public decimal UnitPriceExVat { get; set; }
+    public decimal UnitVatAmount { get; set; }
+    public decimal UnitPriceIncVat { get; set; }
+
+    public decimal LineTotalExVat { get; set; }
+    public decimal LineTotalVat { get; set; }
+    public decimal LineTotalIncVat { get; set; }
+}
---
+++ Domain/Enums/CustomRequestEnums.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace Domain.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter<CustomRequestStatus>))]
+public enum CustomRequestStatus
+{
+    New = 1,
+    Reviewed = 2,
+    Quoted = 3,
+    Closed = 4,
+    Rejected = 5
+}
+[JsonConverter(typeof(JsonStringEnumConverter<CustomQuoteStatus>))]
+public enum CustomQuoteStatus
+{
+    Draft = 1,
+    Sent = 2,
+    Accepted = 3,
+    Rejected = 4,
+    Expired = 5,
+}
