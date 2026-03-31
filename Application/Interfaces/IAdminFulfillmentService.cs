def2984d48319d34e5edf2300d50567906b2f950
Villiamremote <willyp@hotmail.se>
2026-03-31 11:58:29 +0200

    Added Generic PagnationDto

---
+++ Application/Interfaces/IAdminFulfillmentService.cs
@@ -0,0 +1,11 @@
+﻿using Application.DTOs.Admin;
+
+namespace Application.Interfaces;
+
+public interface IAdminFulfillmentService
+{
+    Task<IReadOnlyList<AdminFulfillmentOrderDto>> GetQueueAsync(bool overdueOnly, CancellationToken ct = default);
+    Task<AdminFulfillmentDashboardDto> GetDashboardAsync(CancellationToken ct = default);
+    Task MarkFulfilledAsync(int OrderId, string? note, CancellationToken ct = default);
+    Task ReopenAsync(int orderId, string? note, CancellationToken ct = default);
+}
