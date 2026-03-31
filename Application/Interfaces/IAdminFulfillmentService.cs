55d1748e7f827d09dabd9e2a419c17ace8bb7486
Villiamremote <willyp@hotmail.se>
2026-03-31 12:00:46 +0200

    Updated Interface.

--- Application/Interfaces/IAdminFulfillmentService.cs
+++ Application/Interfaces/IAdminFulfillmentService.cs
@@ -1,11 +1,33 @@
-﻿using Application.DTOs.Admin;
+﻿using Application.DTOs;
+using Application.DTOs.Admin;
 
 namespace Application.Interfaces;
 
 public interface IAdminFulfillmentService
 {
-    Task<IReadOnlyList<AdminFulfillmentOrderDto>> GetQueueAsync(bool overdueOnly, CancellationToken ct = default);
-    Task<AdminFulfillmentDashboardDto> GetDashboardAsync(CancellationToken ct = default);
-    Task MarkFulfilledAsync(int OrderId, string? note, CancellationToken ct = default);
-    Task ReopenAsync(int orderId, string? note, CancellationToken ct = default);
+    Task<PagedResultDto<AdminFulfillmentOrderDto>> GetQueueAsync(
+          FulfillmentQueueFilterDto filter,
+          CancellationToken ct = default);
+
+    Task<AdminFulfillmentDashboardDto> GetDashboardAsync(
+        CancellationToken ct = default);
+
+    Task<AdminFulfillmentOrderDto?> GetByIdAsync(
+        int orderId,
+        CancellationToken ct = default);
+
+    Task MarkFulfilledAsync(
+        int orderId,
+        string? note,
+        CancellationToken ct = default);
+
+    Task ReopenAsync(
+        int orderId,
+        string? note,
+        CancellationToken ct = default);
+
+    Task SetFulfillmentNoteAsync(
+        int orderId,
+        string? note,
+        CancellationToken ct = default);
 }
