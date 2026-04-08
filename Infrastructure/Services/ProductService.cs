aa26f731a952b816a210d4b5ddf064b86b44dd7a
Villiamremote <willyp@hotmail.se>
2026-04-08 11:21:40 +0200

    Changed mapping to correct image.

--- Infrastructure/Services/ProductService.cs
+++ Infrastructure/Services/ProductService.cs
@@ -266,7 +266,7 @@ public class ProductService(
                 p.Images
                     .OrderByDescending(i => i.IsPrimary)
                     .ThenBy(i => i.SortOrder)
-                    .Select(i => i.CardUrl)
+                    .Select(i => i.ThumbUrl)
                     .FirstOrDefault() ?? "",
                 p.Slug ?? "",
                 p.Sku ?? ""))
