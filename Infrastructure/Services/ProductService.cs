0137b7f4c54fa3a323cd9d4ef9348aba3198657f
Villiamremote <willyp@hotmail.se>
2026-04-13 09:16:12 +0200

    Updated compose.

--- Infrastructure/Services/ProductService.cs
+++ Infrastructure/Services/ProductService.cs
@@ -266,7 +266,7 @@ public class ProductService(
                 p.Images
                     .OrderByDescending(i => i.IsPrimary)
                     .ThenBy(i => i.SortOrder)
-                    .Select(i => i.Url)
+                    .Select(i => i.ThumbUrl)
                     .FirstOrDefault() ?? "",
                 p.Slug ?? "",
                 p.Sku ?? ""))
